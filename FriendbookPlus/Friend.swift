bafd1201ddafcb96da73e732987ce20d13083e9d
Toti Cuervo <totifercuervo@gmail.com>
2020-03-28 03:00:23 -0500

    first commit

---
+++ FriendbookPlus/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  FriendbookPlus
+//
+//  Created by Toti Cuervo on 3/26/20.
+//  Copyright © 2020 Toti Cuervo. All rights reserved.
+//
+
+import Foundation
---
+++ FriendbookPlus/FriendDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendDetailViewController.swift
+//  FriendbookPlus
+//
+//  Created by Toti Cuervo on 3/26/20.
+//  Copyright © 2020 Toti Cuervo. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
